abad292acb464fd459f80fe05bb384467ffef7c9
Tamas Barasz <barasztamas@yahoo.com>
2024-09-02 03:44:04 +0200

    check for script error, fix flaky test

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -14,10 +14,10 @@ test.beforeEach(async ({ page }) => {
     const speedSection = page.locator('div.container > div.challenge > h3.right');
     const increaseButton = speedSection.locator('i.timescale_increase');
     const speedDisplay = speedSection.locator('span.emphasis-color');
-    expect(speedDisplay).toHaveText(/^[0-9]*x$/);
-    while (parseInt(await speedDisplay.innerText(), 10) < 1) {
+    await expect(speedDisplay).toHaveText(/^[0-9]+x$/);
+    while (parseInt(await speedDisplay.innerText(), 10) < 20) {
         await increaseButton.click();
-        expect(speedDisplay).toHaveText(/^[0-9]*x$/);
+        await expect(speedDisplay).toHaveText(/^[0-9]*x$/);
     }
 });
 
@@ -29,5 +29,8 @@ Array.from({ length: 2 }, (_, i) => i + 1).forEach((level) => {
         await codebox.press('ControlOrMeta+a');
         await codebox.press('ControlOrMeta+v');
         await page.getByRole('button', { name: 'Apply' }).click();
+        await page.waitForLoadState();
+        const errorBlock = page.locator('div.container > div.codestatus > h5.error');
+        await expect(errorBlock).toHaveText(/^ There is a problem with your code: $/);
     });
 });
