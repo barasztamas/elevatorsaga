7cb00688b44c20eb9aea2a1bb40c3b1c499534a0
Tamas Barasz <barasztamas@yahoo.com>
2024-10-27 07:40:16 +0100

    use chrome for tests

--- playwright.config.js
+++ playwright.config.js
@@ -43,10 +43,10 @@ module.exports = defineConfig({
         //   use: { ...devices['Desktop Firefox'] },
         // },
 
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
-        },
+        // {
+        //     name: 'webkit',
+        //     use: { ...devices['Desktop Safari'] },
+        // },
 
         /* Test against mobile viewports. */
         // {
@@ -63,10 +63,10 @@ module.exports = defineConfig({
         //   name: 'Microsoft Edge',
         //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
         // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        // },
+        {
+            name: 'Google Chrome',
+            use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        },
     ],
 
     /* Run your local dev server before starting the tests */
