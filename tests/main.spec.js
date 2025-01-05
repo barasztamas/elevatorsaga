3d1e9f6d47a07fe7bc8d8e6b0c5ba0a247b1e5a7
Tamas Barasz <barasztamas@yahoo.com>
2025-01-05 11:00:13 +0100

    refactor

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -26,10 +26,11 @@ levelNumbers.forEach((level) => {
     test(`level ${level}`, async ({ page }) => {
         test.setTimeout(5 * 60 * 1000);
         await page.goto(`/#challenge=${level}`);
-        const codebox = page.locator('div.CodeMirror-code > div > pre > span').last();
-        await codebox.click();
-        await codebox.press('ControlOrMeta+a');
-        await pushTextWithBrackets(scriptTexts.default, '{([', codebox);
+        const codebox = page.locator('div.CodeMirror-code > div > pre > span');
+        const codeboxEnd = codebox.last();
+        await codeboxEnd.click();
+        await codeboxEnd.press('ControlOrMeta+a');
+        await pushTextWithBrackets(scriptTexts.default, codeboxEnd);
         await page.getByRole('button', { name: 'Apply' }).click();
         await page.waitForLoadState();
         const errorBlock = page.locator('div.container > div.codestatus > h5.error');
@@ -41,7 +42,7 @@ levelNumbers.forEach((level) => {
     });
 });
 
-async function pushTextWithBrackets(text, brackets, locator) {
+async function pushTextWithBrackets(text = '', locator, brackets = '{([') {
     if (brackets.length === 0) {
         await locator.pressSequentially(text);
         return;
@@ -49,7 +50,7 @@ async function pushTextWithBrackets(text, brackets, locator) {
     const chunks = text.split(brackets[0]);
     for (let i = 0; i < chunks.length; i++) {
         const chunk = chunks[i];
-        await pushTextWithBrackets(chunk, brackets.slice(1), locator);
+        await pushTextWithBrackets(chunk, locator, brackets.slice(1));
         if (i < chunks.length - 1) {
             await locator.press(brackets[0]);
             await locator.press('Delete');
