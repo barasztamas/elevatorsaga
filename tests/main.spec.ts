4c1a0ab19a9cf27a053bca00dff9dfd3fd5b4a2c
Tamas Barasz <barasztamas@yahoo.com>
2025-01-05 19:00:57 +0100

    convert to ts, fix empty lines and excessive indent

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,13 +1,15 @@
-import { expect, test } from '@playwright/test';
+import { expect, Locator, test } from '@playwright/test';
 import fs from 'node:fs';
 import { rootPath } from '../path';
 
-const scriptTexts = {};
+const scriptTexts: { [key: string]: string } = {};
+const scriptTextArrays: { [key: string]: string[] } = {};
 const runSpeed = 20;
-const levelNumbers = Array.from({ length: 2 }, (_, i) => i + 1);
+const levelNumbers = Array.from({ length: 5 }, (_, i) => i + 1);
 
 test.beforeAll('readScript', async () => {
     scriptTexts.default = fs.readFileSync(`${rootPath}/solutions/default.js`, 'utf8');
+    scriptTextArrays.default = scriptTexts.default.split('\n');
 });
 
 test.beforeEach('setSpeed', async ({ page }) => {
@@ -31,29 +33,41 @@ levelNumbers.forEach((level) => {
         await codeboxEnd.click();
         await codeboxEnd.press('ControlOrMeta+a');
         await codeboxEnd.press('Delete');
-        await pushTextWithBrackets(scriptTexts.default, codeboxEnd);
+        await pushCodeToCodeBox(scriptTextArrays.default, codeboxEnd);
         await page.getByRole('button', { name: 'Apply' }).click();
         await page.waitForLoadState();
         const errorBlock = page.locator('div.container > div.codestatus > h5.error');
         if ((await errorBlock.evaluate((e) => window.getComputedStyle(e).getPropertyValue('display'))) !== 'none') {
-            await test.info().attach('error', { body: await errorBlock.textContent() });
-            throw 'Syntax error in code';
+            await test.info().attach('code', { body: (await codebox.allTextContents()).join('\n') });
+            await test.info().attach('error', { body: (await errorBlock.textContent()) ?? undefined });
         }
-        expect(errorBlock).toHaveCSS('display', 'none', { timeout: 10 });
+        expect(errorBlock, 'Syntax error in code').toHaveCSS('display', 'none', { timeout: 10 });
+        const feedback = page.locator('div.container > div.world > div.feedbackcontainer > div.feedback');
+        expect(feedback, 'Failed level').toContainText('Success!');
     });
 });
 
-async function pushTextWithBrackets(text = '', locator, brackets = '{([') {
+async function pushCodeToCodeBox(textArray: string[], locator: Locator) {
+    for (const line of textArray) {
+        await locator.press('Shift+Home');
+        await locator.press('Delete');
+        await pushLineWithBrackets(line, locator);
+    }
+}
+
+async function pushLineWithBrackets(line = '', locator: Locator, brackets = '{([') {
     if (brackets.length === 0) {
-        await locator.pressSequentially(text);
+        await locator.pressSequentially(line);
         return;
     }
-    const chunks = text.split(brackets[0]);
+    const actualBracket = brackets[0];
+    const furtherBrackets = brackets.slice(1);
+    const chunks = line.split(actualBracket);
     for (let i = 0; i < chunks.length; i++) {
         const chunk = chunks[i];
-        await pushTextWithBrackets(chunk, locator, brackets.slice(1));
+        await pushLineWithBrackets(chunk, locator, furtherBrackets);
         if (i < chunks.length - 1) {
-            await locator.press(brackets[0]);
+            await locator.press(actualBracket);
             await locator.press('Delete');
         }
     }
